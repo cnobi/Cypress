d2cd8ea3cd183340d9586eaf60be5d3e8870cc8a
Susan Shrestha <shr.susan7@gmail.com>
2025-02-04 22:29:51 +0545

    Added a sample test at Google.com

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test', () => {
+  it('To see if a condition passes', () => {
+    cy.visit('/')
+    cy.get('#APjFqb').type('Cypress typing test')
+    cy.get('#APjFqb').should('have.value', 'Cypress typing test')
+    cy.get('#APjFqb').clear()
+    cy.get('#APjFqb').type('Cypress Automation Basics for dummies')
+    cy.get('.FPdoLc > center > .gNO89b').click()
+    cy.get('.recaptcha-checkbox-border').wait(5000).click()
   })
 })
