d5abc88cf955ed12dd4f80a042f510e3cfd874f9
Susan Shrestha <shr.susan7@gmail.com>
2025-02-06 22:06:29 +0545

    added pageObjects folder and js file

--- cypress/pageObjects/visit.js
+++ cypress/pageObjects/visit.js
@@ -1,10 +1,10 @@
-describe('Test', () => {
-  it('To see if a condition passes', () => {
+
+
     cy.visit('/')
     cy.get('#APjFqb').type('Cypress typing test')
     cy.get('#APjFqb').should('have.value', 'Cypress typing test')
     cy.get('#APjFqb').clear()
     cy.get('#APjFqb').type('Cypress Automation Basics for dummies')
     cy.get('.FPdoLc > center > .gNO89b').wait(4000).click()
-  })
-})
+
+    export default test()
